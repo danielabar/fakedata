79cc591ee46c371bb076c97306fa53ae51e5ef56
Daniela Baron <baron.daniela@gmail.com>
2014-04-12 20:04:36 -0400

    more obj testing

--- test/obj_test.js
+++ test/obj_test.js
@@ -13,6 +13,22 @@ describe('fakeData obj method', function() {
       name: true
     });
     assert.equal(1, result.length);
+    var name = result[0].name;
+    assert(name === 'Jack Franklin' || name === 'Jeffrey Way');
+  });
+
+  it('returns an array of objects with the specific properties', function() {
+    var result = fakeData.obj(1, {
+      name: true,
+      email: true
+    });
+    assert.equal(1, result.length);
+
+    var name = result[0].name;
+    assert(name === 'Jack Franklin' || name === 'Jeffrey Way');
+
+    var email = result[0].email;
+    assert(email === 'test1@example.com' || email === 'test2@example.com');
   });
 
 });
